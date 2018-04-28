729df79e11d7ba1d0f68b05a30847bf8ef26817a
Garrett Criss <astrolope@gmail.com>
2018-04-28 13:56:22 -0700

    Add src and test trees.

---
+++ test/test.js
@@ -0,0 +1,30 @@
+var expect = require('chai').expect;
+
+//Tests for calculate scores. 
+describe('calculateScores', function () {
+  it('should recieve a team and score', function () {
+    
+
+  });
+
+  it('should order by score', function () {
+    
+
+
+  });
+
+  it('should return scores', function () {
+    
+
+
+  });
+});
+
+//Processfile test handling
+describe('processFile', function () {
+    it('should input a txt file', function () {
+      
+
+    });
+  
+  });
